d390e4ac3bbd2eb805d22110d5772d170a30cd0e
“Nicole <itsnicoleduffy@gmail.com>
2023-09-08 14:51:02 +0200

    final

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,68 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+    constructor (health, strength) {
+        this.health = health;
+        this.strength = strength;    
+    }
+
+    attack (){
+        return this.strength;
+    }
+
+    receiveDamage (damage) {
+
+        this.health = this.health - damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking {
+
+    constructor (name, health, strength) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }    
+    
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+
+        this.health = this.health - damage
+    }
+
+    battleCry (){
+        return "Odin Owns You All!"
+    }
+
+}
+
+
 
 // Saxon
-class Saxon {}
+class Saxon {
+    
+    constructor (health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack () {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage
+    }
+
+}
+
+
 
 // War
-class War {}
+class War {
+
+}
